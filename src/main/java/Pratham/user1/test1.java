7b6cd65b5518d1d357e33fb94dc24a951826d85d
someone <prathmesh.s.kshirsagar@gmail.com>
2023-02-17 20:57:56 +0530

    This Msg is commited by intelij

--- src/main/java/Pratham/user1/test1.java
+++ src/main/java/Pratham/user1/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Welcome To Testing");
         System.out.println("Thank You");
+        System.out.println("Commit using Intellij");
     }
 }
